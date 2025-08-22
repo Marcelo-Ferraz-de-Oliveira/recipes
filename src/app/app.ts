33bbc553c3ee317d5352941aaf28a838f8f1cd94
Marcelo Ferraz de Oliveira <mfoliveira@tce.sp.gov.br>
2025-08-22 15:04:13 -0300

    Capítulo 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/ui/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'recipes';
+  protected title = 'My Recipe Book';
 }
